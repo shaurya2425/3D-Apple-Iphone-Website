17aeadd9125cd49d56dc7db8d70bc7814cc43117
shaurya2425 <askshauryarajput@gmail.com>
2025-05-30 10:09:45 +0530

    Setup

---
+++ 3D-Apple-Iphone-Website/postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: c:\Users\shury\OneDrive\Desktop\CODING\MERN PROJECTS\REACT PROJECTS\3D-Apple-Iphone-Website\3D-Apple-Iphone-Website\postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
